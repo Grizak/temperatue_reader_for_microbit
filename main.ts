69b01496c8a2ee866163c086cbe12780af26e369
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2025-05-14 11:59:07 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,3 +18,16 @@ input.onButtonPressed(Button.A, () => {
     showTemp();
     loops.everyInterval(10 * 1000, showTemp);
 });
+
+const showTempF = () => {
+    basic.showString("Temp:")
+    basic.showNumber(input.temperature() + 273.15);
+    basic.showString("K")
+    basic.pause(1000)
+    basic.clearScreen();
+}
+
+input.onButtonPressed(Button.B, () => {
+    showTempF();
+    loops.everyInterval(10*1000, showTempF)
+})
